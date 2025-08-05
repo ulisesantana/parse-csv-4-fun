e4581b853e24255e416ae74cf7f20461ab4987aa
Ulises Santana <ulisesantana@gmail.com>
2025-08-05 08:13:07 +0100

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import markdown from "@eslint/markdown";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import markdown from '@eslint/markdown';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js, prettier },
+    extends: ['js/recommended', prettierConfig],
+    rules: { 'prettier/prettier': 'error' },
+  },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/commonmark',
+    extends: ['markdown/recommended'],
+  },
 ]);
